65e4281344e8ee4c2b2a64d81e369e9aeab3d3cc
sachin3698k <hiranmay864@gmail.com>
2022-12-01 18:46:42 +0530

    Firebase

---
+++ config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKdoPcIzTC2WbTwyWNr0ALXi-sHiqcJH4",
+  authDomain: "npmjs-product-auth.firebaseapp.com",
+  projectId: "npmjs-product-auth",
+  storageBucket: "npmjs-product-auth.appspot.com",
+  messagingSenderId: "595462551354",
+  appId: "1:595462551354:web:0de3823c2440241945c203",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
