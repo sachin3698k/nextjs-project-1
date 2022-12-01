3e25d3ed4634b828b13eabd1d6ad35c41aa4a1c9
sachin3698k <hiranmay864@gmail.com>
2022-12-01 18:48:33 +0530

    firebase edit

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAKdoPcIzTC2WbTwyWNr0ALXi-sHiqcJH4",
   authDomain: "npmjs-product-auth.firebaseapp.com",
@@ -13,5 +9,4 @@ const firebaseConfig = {
   appId: "1:595462551354:web:0de3823c2440241945c203",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
